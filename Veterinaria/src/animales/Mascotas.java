16e102c0850ad64f2d1471e4c92d13f9a6351848
yorlinisyanceg-cell <yorlinis.yance.g@unicorsalud.edu.co>
2026-03-10 08:39:49 -0500

    Add files via upload

---
+++ Veterinaria/src/animales/Mascotas.java
@@ -0,0 +1,14 @@
+package animales;
+
+public class Mascotas {
+    private String nombre;
+    private String propietario;
+    private animal animal;
+
+    public mascota(String nombre,String propietario,animal animal) {
+        this.nombres = nombre;
+        this.propietario = propietario;
+        this.animal = animal;
+    }
+
+}
---
+++ Veterinaria/src/ejecucion/Principal.java
@@ -0,0 +1,15 @@
+package ejecucion;
+
+public class Animal  {
+    private String tipo;
+    private String raza;
+    private int edad;
+
+    public Animal (String tipo, String raza, int edad){
+        this. tipo = tipo;
+        this. raza = raza;
+        this. edad = edad;
+
+    }
+    public
+}
